9512df1eb2c536ab800b40994f70d2b85f533cba
AlexMato <matontialessandro0@gmail.com>
2020-04-30 20:53:56 +0200

    ultime rifiniture

--- dist/app.js
+++ dist/app.js
@@ -94,6 +94,18 @@
 /***/ (function(module, exports) {
 
 console.log("cane");
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    console.log("sto scrollando", scrollTop);
+
+    if (scrollTop > 70) {
+      console.log("fai qualcosa");
+    } else {
+      console.log("nascondo");
+    }
+  });
+});
 
 /***/ }),
 
--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 console.log("cane");
+
+
+$(document).ready(function() {
+
+  $(window).scroll(function() {
+
+    var scrollTop = $(this).scrollTop();
+
+    console.log("sto scrollando", scrollTop);
+
+    if (scrollTop > 70) {
+      console.log("fai qualcosa");
+    } else {
+      console.log("nascondo");
+    }
+  });
+});

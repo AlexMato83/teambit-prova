b1e53bab36a3fb97831fb026c19da6e52da23147
AlexMato <matontialessandro0@gmail.com>
2020-05-01 20:46:30 +0200

    alternanza header

--- dist/app.js
+++ dist/app.js
@@ -95,16 +95,22 @@
 
 console.log("cane");
 $(document).ready(function () {
+  $("#header2").hide();
   $(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
     console.log("sto scrollando", scrollTop);
 
-    if (scrollTop > 70) {
-      console.log("fai qualcosa");
+    if (scrollTop > 140) {
+      // $( "#book" ).slideDown( "slow", function())
+      $("#header2").show();
+      $("#header2").slideDown("slow");
     } else {
-      console.log("nascondo");
+      $("header").show();
+      $("#header2").hide();
     }
   });
+
+  function animation() {}
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ console.log("cane");
 
 
 $(document).ready(function() {
-
+  $("#header2").hide();
   $(window).scroll(function() {
 
     var scrollTop = $(this).scrollTop();
 
     console.log("sto scrollando", scrollTop);
 
-    if (scrollTop > 70) {
-      console.log("fai qualcosa");
+    if (scrollTop > 140) {
+      // $( "#book" ).slideDown( "slow", function())
+      $("#header2").show();
+      $("#header2").slideDown("slow");
+
     } else {
-      console.log("nascondo");
+      $("header").show();
+      $("#header2").hide();
     }
   });
+  function animation() {
+
+  }
 });
